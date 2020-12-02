4b30f3dba347095b4c90a3ff9d38877b4643f69e
Nseghe <essietnseghe@gmail.com>
2020-12-02 09:48:10 +0100

    Fixed Multer upload issue

--- api/src/images/controllers/images.controller.js
+++ api/src/images/controllers/images.controller.js
@@ -7,18 +7,23 @@ class imagesController {
   }
 
   upload(req, res) {
+
     this.imagesModel.uploadImage(req, res).then((data) => {
+      
       console.log(`Uploaded image ${data.Key} to s3 bucket`);
+      
       this.imagesModel.uploadImageDetails(req, data).then(() => {
         console.log('Uploaded image details to RDS');
         return res.status(201).send({success: 'Image uploaded successfully'});
       }).catch((error) => {
         console.log('RDS error');
+        
         this.imagesModel.deleteImage(data).then(() => {
           console.log(`Deleted image ${data.Key} from s3 bucket`);
           return res.status(500).send({error: `Unable to upload image details to RDS. Deleted image ${data.key} from S3 bucket. Please try again.`});
         }).catch((err) => {
           return res.status(500).send({error: `Unable to delete image ${data.key} from S3 bucket. Image ${data.key} should be deleted manually.`});
+        
         })
       })
     }).catch((err) => {
--- api/src/initialize.app.js
+++ api/src/initialize.app.js
@@ -6,7 +6,6 @@ function initialize({
 }) {
   app.use(bodyParser.json());
   app.use(function (req, res, next) {
-    // res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST');
     res.header('Access-Control-Allow-Headers', 'Accept, Content-Type');
     if (req.method === 'OPTIONS') {
